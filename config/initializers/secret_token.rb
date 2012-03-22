43dc62bcfd2434dc084efbabad183cb2403e944b
Joe Sak <joe@joesak.com>
2012-03-21 20:09:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WannaLearn::Application.config.secret_token = 'ab92a279649d4fa41903ddd27f1f986476177bfd4e3d1bb0a9b977f37a28bfb658f208cdb932b186c314b94c57ba8fc9946ae5168ff8e3a49e644429112fe089'
